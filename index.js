f4c60cf33f2ab6e11c37cc4784c98cace75d0621
Mees Maas <meesmaas999@gmail.com>
2021-12-09 20:03:32 +0100

    MotionSensor

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const AirPurifierAccessory = require('./lib/air_purifier_accessory')
 const WindowCoveringAccessory = require('./lib/window_covering_accessory')
 const ContactSensorAccessory = require('./lib/contactsensor_accessory');
 const LeakSensorAccessory = require('./lib/leak_sensor_accessory')
+const MotionSensorAccessory = require('./lib/motion_sensor_accessory')
 
 const LogUtil = require('./util/logutil')
 const DataUtil = require('./util/datautil')
@@ -183,6 +184,10 @@ class TuyaPlatform {
         this.accessories.set(uuid, deviceAccessory.homebridgeAccessory);
         this.deviceAccessories.set(uuid, deviceAccessory);
         break;
+      case 'pir':
+        deviceAccessory = new MotionSensorAccessory(this, homebridgeAccessory, device);
+        this.accessories.set(uuid, deviceAccessory.homebridgeAccessory);
+        this.deviceAccessories.set(uuid, deviceAccessory);
       default:
         break;
     }
