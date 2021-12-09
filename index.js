72708d7a3e75e899c10abee20ebe4fc1dde59e13
Mees Maas <meesmaas999@gmail.com>
2021-12-09 20:11:41 +0100

    MotionSensor

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const AirPurifierAccessory = require('./lib/air_purifier_accessory')
 const WindowCoveringAccessory = require('./lib/window_covering_accessory')
 const ContactSensorAccessory = require('./lib/contactsensor_accessory');
 const LeakSensorAccessory = require('./lib/leak_sensor_accessory')
-const MotionSensorAccessory = require('./lib/motion_sensor_accessory')
+const MotionSensorAccessory = require('./lib/motionsensor_accessory')
 
 const LogUtil = require('./util/logutil')
 const DataUtil = require('./util/datautil')
