f5162dee4c27dbd34af6a4d7a09b1f4100a34738
gaosen <0x5e@sina.cn>
2022-11-05 11:36:56 +0800

    Support HumiditySensor of schema 'humidity_value' (#45)

--- src/accessory/TemperatureHumiditySensorAccessory.ts
+++ src/accessory/TemperatureHumiditySensorAccessory.ts
@@ -8,41 +8,56 @@ export default class TemperatureHumiditySensorAccessory extends BaseAccessory {
   constructor(platform: TuyaPlatform, accessory: PlatformAccessory) {
     super(platform, accessory);
 
-    if (this.getStatus('va_temperature')) {
-      const service = this.accessory.getService(this.Service.TemperatureSensor)
-        || this.accessory.addService(this.Service.TemperatureSensor);
+    this.configureTemperatureSensor();
+    this.configureHumiditySensor();
+  }
 
-      const property = this.getSchema('va_temperature')?.property as TuyaDeviceSchemaIntegerProperty;
-      const multiple = Math.pow(10, property ? property.scale : 0);
-      service.getCharacteristic(this.Characteristic.CurrentTemperature)
-        .onGet(() => {
-          const status = this.getStatus('va_temperature');
-          this.log.debug('CurrentTemperature:', 'property =', property, 'multiple =', multiple, 'status =', status);
-          let temperature = status!.value as number / multiple;
-          temperature = Math.max(-270, temperature);
-          temperature = Math.min(100, temperature);
-          return temperature;
-        });
-
+  configureTemperatureSensor() {
+    const schema = this.getSchema('va_temperature');
+    if (!schema) {
+      this.log.warn('TemperatureSensor not supported.');
+      return;
     }
 
-    if (this.getStatus('va_humidity')) {
-      const service = this.accessory.getService(this.Service.HumiditySensor)
-        || this.accessory.addService(this.Service.HumiditySensor);
+    const service = this.accessory.getService(this.Service.TemperatureSensor)
+      || this.accessory.addService(this.Service.TemperatureSensor);
 
-      const property = this.getSchema('va_humidity')?.property as TuyaDeviceSchemaIntegerProperty;
-      const multiple = Math.pow(10, property ? property.scale : 0);
-      service.getCharacteristic(this.Characteristic.CurrentRelativeHumidity)
-        .onGet(() => {
-          const status = this.getStatus('va_humidity');
-          this.log.debug('CurrentRelativeHumidity:', 'property =', property, 'multiple =', multiple, 'status =', status);
-          let humidity = Math.floor(status!.value as number / multiple);
-          humidity = Math.max(0, humidity);
-          humidity = Math.min(100, humidity);
-          return humidity;
-        });
+    const property = schema.property as TuyaDeviceSchemaIntegerProperty;
+    const multiple = Math.pow(10, property ? property.scale : 0);
+    service.getCharacteristic(this.Characteristic.CurrentTemperature)
+      .onGet(() => {
+        const status = this.getStatus(schema.code);
+        this.log.debug('CurrentTemperature:', 'property =', property, 'multiple =', multiple, 'status =', status);
+        let temperature = status!.value as number / multiple;
+        temperature = Math.max(-270, temperature);
+        temperature = Math.min(100, temperature);
+        return temperature;
+      });
 
+  }
+
+  configureHumiditySensor() {
+    const schema = this.getSchema('va_humidity')
+      || this.getSchema('humidity_value');
+    if (!schema) {
+      this.log.warn('HumiditySensor not supported.');
+      return;
     }
+
+    const service = this.accessory.getService(this.Service.HumiditySensor)
+      || this.accessory.addService(this.Service.HumiditySensor);
+
+    const property = schema.property as TuyaDeviceSchemaIntegerProperty;
+    const multiple = Math.pow(10, property ? property.scale : 0);
+    service.getCharacteristic(this.Characteristic.CurrentRelativeHumidity)
+      .onGet(() => {
+        const status = this.getStatus(schema.code);
+        this.log.debug('CurrentRelativeHumidity:', 'property =', property, 'multiple =', multiple, 'status =', status);
+        let humidity = Math.floor(status!.value as number / multiple);
+        humidity = Math.max(0, humidity);
+        humidity = Math.min(100, humidity);
+        return humidity;
+      });
 
   }
 
