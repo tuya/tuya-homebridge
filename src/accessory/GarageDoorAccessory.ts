1b941e8c2744fc800ac0626c1eed27deca58119d
gaosen <0x5e@sina.cn>
2022-11-07 19:02:11 +0800

    Skip for non-standard garage door accessory.

--- src/accessory/GarageDoorAccessory.ts
+++ src/accessory/GarageDoorAccessory.ts
@@ -10,38 +10,41 @@ export default class GarageDoorAccessory extends BaseAccessory {
     const service = this.accessory.getService(this.Service.GarageDoorOpener)
       || this.accessory.addService(this.Service.GarageDoorOpener);
 
+    const { OPEN, CLOSED, OPENING, CLOSING, STOPPED } = this.Characteristic.CurrentDoorState;
     service.getCharacteristic(this.Characteristic.CurrentDoorState)
       .onGet(() => {
-        const currentStatus = this.getStatus('doorcontact_state')!;
-        const targetStatus = this.getStatus('switch_1')!;
-
-        if (currentStatus.value === true && targetStatus.value === true) {
-          return this.Characteristic.CurrentDoorState.OPEN;
-        } else if (currentStatus.value === false && targetStatus.value === false) {
-          return this.Characteristic.CurrentDoorState.CLOSED;
-        } else if (currentStatus.value === false && targetStatus.value === true) {
-          return this.Characteristic.CurrentDoorState.OPENING;
-        } else if (currentStatus.value === true && targetStatus.value === false) {
-          return this.Characteristic.CurrentDoorState.CLOSING;
+        const currentStatus = this.getStatus('doorcontact_state');
+        const targetStatus = this.getStatus('switch_1');
+        if (!currentStatus || !targetStatus) {
+          return STOPPED;
         }
 
-        return this.Characteristic.CurrentDoorState.STOPPED;
+        if (currentStatus.value === true && targetStatus.value === true) {
+          return OPEN;
+        } else if (currentStatus.value === false && targetStatus.value === false) {
+          return CLOSED;
+        } else if (currentStatus.value === false && targetStatus.value === true) {
+          return OPENING;
+        } else if (currentStatus.value === true && targetStatus.value === false) {
+          return CLOSING;
+        }
 
+        return STOPPED;
       });
 
-    service.getCharacteristic(this.Characteristic.TargetDoorState)
-      .onGet(() => {
-        const status = this.getStatus('switch_1')!;
-        return status.value ?
-          this.Characteristic.TargetDoorState.OPEN :
-          this.Characteristic.TargetDoorState.CLOSED;
-      })
-      .onSet(value => {
-        this.sendCommands([{
-          code: 'switch_1',
-          value: (value === this.Characteristic.TargetDoorState.OPEN) ? true : false,
-        }]);
-      });
+    if (this.getStatus('switch_1')) {
+      service.getCharacteristic(this.Characteristic.TargetDoorState)
+        .onGet(() => {
+          const status = this.getStatus('switch_1')!;
+          return status.value ? OPEN : CLOSED;
+        })
+        .onSet(value => {
+          this.sendCommands([{
+            code: 'switch_1',
+            value: (value === OPEN) ? true : false,
+          }]);
+        });
+    }
 
   }
 
