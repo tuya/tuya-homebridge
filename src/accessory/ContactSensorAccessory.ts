2d7346e0fe60c139747ec5403d9d62a6b0f061da
gaosen <0x5e@sina.cn>
2022-11-13 20:12:58 +0800

    Fix Contact Sensor not working (#75)

--- src/accessory/ContactSensorAccessory.ts
+++ src/accessory/ContactSensorAccessory.ts
@@ -7,17 +7,25 @@ export default class ContaceSensor extends BaseAccessory {
   constructor(platform: TuyaPlatform, accessory: PlatformAccessory) {
     super(platform, accessory);
 
-    const service = this.accessory.getService(this.Service.ContactSensor)
+    const schema = this.getContactSensorSchema();
+    if (schema) {
+      const service = this.accessory.getService(this.Service.ContactSensor)
       || this.accessory.addService(this.Service.ContactSensor);
 
-    service.getCharacteristic(this.Characteristic.ContactSensorState)
-      .onGet(() => {
-        const status = this.getStatus('doorcontact_state');
-        return status!.value ?
-          this.Characteristic.ContactSensorState.CONTACT_NOT_DETECTED:
-          this.Characteristic.ContactSensorState.CONTACT_DETECTED;
-      });
+      const { CONTACT_NOT_DETECTED, CONTACT_DETECTED } = this.Characteristic.ContactSensorState;
+      service.getCharacteristic(this.Characteristic.ContactSensorState)
+        .onGet(() => {
+          const status = this.getStatus(schema.code)!;
+          return status.value ? CONTACT_NOT_DETECTED : CONTACT_DETECTED;
+        });
+    }
 
+
+  }
+
+  getContactSensorSchema() {
+    return this.getSchema('doorcontact_state')
+      || this.getSchema('switch');
   }
 
 }
