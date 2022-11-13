c0ade62ffc633b535c1faf7cff6ea222258623a2
gaosen <0x5e@sina.cn>
2022-11-13 20:28:29 +0800

    Add battery status code (#76)

--- src/accessory/BaseAccessory.ts
+++ src/accessory/BaseAccessory.ts
@@ -105,7 +105,8 @@ export default class BaseAccessory {
   getBatteryPercentage() {
     return this.getStatus('battery_percentage')
       || this.getStatus('residual_electricity')
-      || this.getStatus('va_battery');
+      || this.getStatus('va_battery')
+      || this.getStatus('battery');
   }
 
   getChargeState() {
