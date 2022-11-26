7200b66ec21a0059b16848545f149e41f548d10a
gaosen <0x5e@sina.cn>
2022-11-26 13:00:11 +0800

    Fix Motion Sensor Light not working. (#122)
    * Fix `pir_state` not working for `gyd`.
    
    * Add some logs
    
    * Fix wrong light type, and add pir switch.

--- src/accessory/LightAccessory.ts
+++ src/accessory/LightAccessory.ts
@@ -13,18 +13,20 @@ const SCHEMA_CODE = {
   COLOR_TEMP: ['temp_value', 'temp_value_v2'],
   COLOR: ['colour_data', 'colour_data_v2'],
   WORK_MODE: ['work_mode'],
+  PIR: ['pir_state'],
+  PIR_ON: ['switch_pir'],
 };
 
 const DEFAULT_COLOR_TEMPERATURE_KELVIN = 6500;
 
 enum LightAccessoryType {
-  Unknown = -1,
-  Normal = 0, // Normal Accessory, similar to SwitchAccessory, OutletAccessory.
-  C = 1, // Accessory with brightness.
-  CW = 2, // Accessory with brightness and color temperature (Cold and Warm).
-  RGB = 3, // Accessory with color (RGB <--> HSB).
-  RGBC = 4, // Accessory with color and brightness.
-  RGBCW = 5, // Accessory with color, brightness and color temperature (two work mode).
+  Unknown = 'Unknown',
+  Normal = 'Normal', // Normal Accessory, similar to SwitchAccessory, OutletAccessory.
+  C = 'C', // Accessory with brightness.
+  CW = 'CW', // Accessory with brightness and color temperature (Cold and Warm).
+  RGB = 'RGB', // Accessory with color (RGB <--> HSB).
+  RGBC = 'RGBC', // Accessory with color and brightness.
+  RGBCW = 'RGBCW', // Accessory with color, brightness and color temperature (two work mode).
 }
 
 type TuyaDeviceSchemaColorProperty = {
@@ -42,7 +44,10 @@ export default class LightAccessory extends BaseAccessory {
   ) {
     super(platform, accessory);
 
-    switch (this.getAccessoryType()) {
+    const type = this.getAccessoryType();
+    this.log.info('Light Accessory type:', type);
+
+    switch (type) {
       case LightAccessoryType.Normal:
         configureOn(this, this.getLightService(), this.getSchema(...SCHEMA_CODE.ON));
         break;
@@ -71,7 +76,7 @@ export default class LightAccessory extends BaseAccessory {
         break;
     }
 
-    configureMotionDetected(this);
+    this.configurePIR();
   }
 
   getLightService() {
@@ -94,11 +99,11 @@ export default class LightAccessory extends BaseAccessory {
       accessoryType = LightAccessoryType.RGBC;
     } else if (on && !temp && h && s && v) {
       accessoryType = LightAccessoryType.RGB;
-    } else if (on && bright && temp && !color) {
+    } else if (on && bright && temp) {
       accessoryType = LightAccessoryType.CW;
-    } else if (on && bright && !temp && !color) {
+    } else if (on && bright && !temp) {
       accessoryType = LightAccessoryType.C;
-    } else if (on && !bright && !temp && !color) {
+    } else if (on && !bright && !temp) {
       accessoryType = LightAccessoryType.Normal;
     } else {
       accessoryType = LightAccessoryType.Unknown;
@@ -310,4 +315,16 @@ export default class LightAccessory extends BaseAccessory {
       });
   }
 
+  configurePIR() {
+    const onSchema = this.getSchema(...SCHEMA_CODE.PIR_ON);
+    if (onSchema) {
+      const service = this.accessory.getService(onSchema.code)
+        || this.accessory.addService(this.Service.Switch, onSchema.code, onSchema.code);
+      configureOn(this, service, onSchema);
+    }
+
+    const motionSchema = this.getSchema(...SCHEMA_CODE.PIR);
+    configureMotionDetected(this, undefined, motionSchema);
+
+  }
 }
