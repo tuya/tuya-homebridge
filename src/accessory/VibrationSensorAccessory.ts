b7f656b9c4fa75a5fa14a174055ae33f12bfe894
gaosen <0x5e@sina.cn>
2023-04-13 13:37:36 +0800

    Manually trigger normal state for vibration sensor.

--- src/accessory/VibrationSensorAccessory.ts
+++ src/accessory/VibrationSensorAccessory.ts
@@ -1,3 +1,4 @@
+import { TuyaDeviceStatus } from '../device/TuyaDevice';
 import BaseAccessory from './BaseAccessory';
 
 const SCHEMA_CODE = {
@@ -11,15 +12,38 @@ export default class VibrationSensorAccessory extends BaseAccessory {
   }
 
   configureServices() {
-    const service = this.accessory.getService(this.Service.MotionSensor)
-    || this.accessory.addService(this.Service.MotionSensor);
+    this.getMotionService().setCharacteristic(this.Characteristic.MotionDetected, false);
+  }
 
-    const schema = this.getSchema(...SCHEMA_CODE.STATE)!;
-    service.getCharacteristic(this.Characteristic.MotionDetected)
-      .onGet(() => {
-        const status = this.getStatus(schema.code)!;
-        return status.value !== 'normal';
-      });
+  getMotionService() {
+    return this.accessory.getService(this.Service.MotionSensor)
+      || this.accessory.addService(this.Service.MotionSensor);
+  }
+
+  async onDeviceStatusUpdate(status: TuyaDeviceStatus[]) {
+    super.onDeviceStatusUpdate(status);
+
+    const motionSchema = this.getSchema(...SCHEMA_CODE.STATE)!;
+    const motionStatus = status.find(_status => _status.code === motionSchema.code);
+    motionStatus && this.onMotionDetected(motionStatus);
+  }
+
+  private timer?: NodeJS.Timeout;
+  onMotionDetected(status: TuyaDeviceStatus) {
+    if (!this.intialized) {
+      return;
+    }
+
+    if (status.value !== 'vibration' && status.value !== 'drop') {
+      return;
+    }
+
+    this.log.info('Motion event:', status.value);
+    const characteristic = this.getMotionService().getCharacteristic(this.Characteristic.MotionDetected);
+    characteristic.updateValue(true);
+
+    this.timer && clearTimeout(this.timer);
+    this.timer = setTimeout(() => characteristic.updateValue(false), 3 * 1000);
   }
 
 }
